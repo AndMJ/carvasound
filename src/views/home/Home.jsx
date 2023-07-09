5527a0c3164efa1692ef7dadeb63d2af1cc20cc5
AndMJ <andrejoao070@gmail.com>
2023-07-09 03:05:21 +0100

    UPDATE carousel working properly

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,6 +1,6 @@
 import "./home.css";
 
-import {Carousel} from "react-bootstrap";
+import {Carousel, Container, Row} from "react-bootstrap";
 
 import carousel1 from "../../assets/img/carousel/carousel1.jpg";
 import carousel2 from "../../assets/img/carousel/carousel2.jpg";
@@ -9,31 +9,28 @@ import carousel4 from "../../assets/img/carousel/carousel4.jpg";
 
 function Home(){
     return(
-        <>
-            <Carousel fade={true}>
-                <Carousel.Item style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + carousel4 + ")"}}>
-                    <Carousel.Caption>
-                        <h1>Som profissional</h1>
-                        <h4>Escolha a sua musica para um momento especifico</h4>
-                    </Carousel.Caption>
-                </Carousel.Item>
+        <Carousel fade={true}>
+            <Carousel.Item style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + carousel4 + ")"}}>
+                <Carousel.Caption>
+                    <h1>Som profissional</h1>
+                    <h4>Escolha a sua musica para um momento especifico</h4>
+                </Carousel.Caption>
+            </Carousel.Item>
 
-                <Carousel.Item style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + carousel3 + ")"}}>
-                    <Carousel.Caption>
-                        <h1>Som profissional</h1>
-                        <h4>Escolha a sua musica para um momento especifico</h4>
-                    </Carousel.Caption>
-                </Carousel.Item>
+            <Carousel.Item style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + carousel3 + ")"}}>
+                <Carousel.Caption>
+                    <h1>Som profissional</h1>
+                    <h4>Escolha a sua musica para um momento especifico</h4>
+                </Carousel.Caption>
+            </Carousel.Item>
 
-                <Carousel.Item style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + carousel2 + ")"}}>
-                    <Carousel.Caption>
-                        <h1>Som profissional</h1>
-                        <h4>Escolha a sua musica para um momento especifico</h4>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-
-        </>
+            <Carousel.Item style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + carousel2 + ")"}}>
+                <Carousel.Caption>
+                    <h1>Som profissional</h1>
+                    <h4>Escolha a sua musica para um momento especifico</h4>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
