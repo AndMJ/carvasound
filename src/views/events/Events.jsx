141864beef64b401963bfff8f02b7f671a448373
AndMJ <andrejoao070@gmail.com>
2023-07-09 15:47:55 +0100

    UPDATE events page

--- src/views/events/Events.jsx
+++ src/views/events/Events.jsx
@@ -2,42 +2,129 @@ import "./events.css";
 import {Col, Container, Image, Row} from "react-bootstrap";
 
 import appLogo from "/logo.svg"
+import eventImage_casamento from "../../assets/img/events/casamento.jpg"
+
 function Events(){
     return(
         <>
-            <Container className={"mt-5"}>
-                <Row className={"text-center"}>
+            <Container>
+                <Row className={"text-center mt-5"}>
                     <h1>Events</h1>
-                </Row>
-                <Row className={"m-5"}>
-                    <Col sm={8} md={8} >
-                        <h2>Event 1</h2>
-                        <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
-                    </Col>
-                    <Col sm={4} md={4}>
-                        <Image src={appLogo} rounded fluid />
-                    </Col>
-                </Row>
-                <Row className={"m-5"}>
-                    <Col sm={4} md={4}>
-                        <Image src={appLogo} rounded fluid />
-                    </Col>
-                    <Col sm={8} md={8} >
-                        <h2>Event 2</h2>
-                        <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
-                    </Col>
-                </Row>
-
-                <Row className={"m-5"}>
-                    <Col sm={8} md={8} >
-                        <h2>Event 3</h2>
-                        <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
-                    </Col>
-                    <Col sm={4} md={4}>
-                        <Image src={appLogo} rounded fluid />
-                    </Col>
+                    <p>Ut tortor pretium viverra suspendisse potenti. Augue interdum velit euismod in. Cras pulvinar mattis nunc sed blandit libero. Pretium quam vulputate dignissim suspendisse in est.</p>
                 </Row>
             </Container>
+
+            <section className={"mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Casamentos</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                        <Col md={6}>
+                            <Image src={eventImage_casamento} rounded fluid />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"bg-light mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Festas temáticas</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Aniversários</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"bg-light mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Palestras</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Workshops</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"bg-light mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Apresentações</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Batizados</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className={"bg-light mt-5 py-5"}>
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <Image src={appLogo} rounded fluid />
+                        </Col>
+                        <Col md={6} className={"event-text"}>
+                            <h2>Eventos online</h2>
+                            <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </>
     )
 }
