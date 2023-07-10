f662dbc4d3fe1924680ef5780b164fea0e08dbb1
AndMJ <andrejoao070@gmail.com>
2023-07-10 20:47:05 +0100

    UPDATE not found page

--- src/views/not_found/NotFound.jsx
+++ src/views/not_found/NotFound.jsx
@@ -1,6 +1,10 @@
 import "./notFound.css"
-import {Container, Row} from "react-bootstrap";
+import {Container, Row, Image, Col} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {FaArrowLeft} from "react-icons/fa";
+
+//assets
+import image404 from "../../assets/img/not_found/page-not-found.png"
 
 function NotFound (){
     return (
@@ -8,8 +12,12 @@ function NotFound (){
             <section>
                 <Container>
                     <Row className={"text-center pt-5"}>
-                        <h1>404 - Not Found</h1>
-                        <Link to={"/"} >Go back to Home</Link>
+                        <Col sm={12}>
+                            <Image src={image404} rounded />
+                        </Col>
+                        <Col sm={12} className={"py-5"}>
+                            <Link to={"/"}> <FaArrowLeft/> Go back to Home</Link>
+                        </Col>
                     </Row>
                 </Container>
             </section>
