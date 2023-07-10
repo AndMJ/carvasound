3418ff45b2244129d27eae1033f1cf490673186a
AndMJ <andrejoao070@gmail.com>
2023-07-10 17:53:18 +0100

    ADD 404 not found route path

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Home from "./views/home/Home.jsx";
 import Events from "./views/events/Events.jsx";
 import Gallery from "./views/gallery/Gallery.jsx";
 import Contact from "./views/contact/Contact.jsx";
+import NotFound from "./views/not_found/NotFound.jsx";
 
 function App() {
 
@@ -18,9 +19,10 @@ function App() {
         <Routes>
             <Route path={"/"} element={<Header />}>
                 <Route index element={<Home />} />
-                <Route path={"events"} element={<Events />}></Route>
-                <Route path={"gallery"} element={<Gallery />}></Route>
-                <Route path={"contact"} element={<Contact />}></Route>
+                <Route path={"/events"} element={<Events />}></Route>
+                <Route path={"/gallery"} element={<Gallery />}></Route>
+                <Route path={"/contact"} element={<Contact />}></Route>
+                <Route path={"*"} element={<NotFound />}></Route>
             </Route>
         </Routes>
     </>
