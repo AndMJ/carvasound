8b26ac6a66830a122198921a3f1d82ac89ddd52f
AndMJ <andrejoao070@gmail.com>
2023-07-10 23:01:02 +0100

    ADD app Layout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import {Route, Routes} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //my components/views
-import Header from "./components/header/Header.jsx";
 import Home from "./views/home/Home.jsx";
 import Events from "./views/events/Events.jsx";
 import Gallery from "./views/gallery/Gallery.jsx";
 import Contact from "./views/contact/Contact.jsx";
 import NotFound from "./views/not_found/NotFound.jsx";
+import Layout from "./components/layout/Layout.jsx";
 
 function App() {
 
   return (
     <>
         <Routes>
-            <Route path={"/"} element={<Header />}>
+            <Route path={"/"} element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path={"/events"} element={<Events />}></Route>
                 <Route path={"/gallery"} element={<Gallery />}></Route>
