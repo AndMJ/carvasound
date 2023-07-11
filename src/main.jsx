a7c6ba06cd5c412a600a7428fddfb204f9f990f6
AndMJ <andrejoao070@gmail.com>
2023-07-11 02:01:29 +0100

    house cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
+
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true }}>
           <App></App>
       </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
