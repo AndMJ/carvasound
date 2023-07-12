a50725aab08e913a0cc274ddaef5e777d48bb5ee
AndMJ <andrejoao070@gmail.com>
2023-07-12 03:15:11 +0100

    UPDATE Events page and styles

--- src/views/user/events/Events.jsx
+++ src/views/user/events/Events.jsx
@@ -6,82 +6,83 @@ import eventImage_casamento from "../../../assets/img/events/casamento.jpg"
 import {Link} from "react-router-dom";
 
 function Events(){
+
     return(
         <>
-            <section className={"py-5"}>
+            <section className={"py-5"} data-aos="fade-up-left">
                 <Container>
-                    <div className={"text-center mb-5"}>
-                        <h2>Events</h2>
-                        {/*<p>Ut tortor pretium viverra suspendisse potenti. Augue interdum velit euismod in. Cras pulvinar mattis nunc sed blandit libero. Pretium quam vulputate dignissim suspendisse in est.</p>*/}
-                    </div>
+                    {/*<div className={"text-center mb-5"}>*/}
+                    {/*    <h2>Events</h2>*/}
+                    {/*    /!*<p>Ut tortor pretium viverra suspendisse potenti. Augue interdum velit euismod in. Cras pulvinar mattis nunc sed blandit libero. Pretium quam vulputate dignissim suspendisse in est.</p>*!/*/}
+                    {/*</div>*/}
                     <Row className={"align-items-center"}>
-                        <Col md={6} className={"text-start px-5 py-3"}>
-                            <h2>Casamentos</h2>
+                        <Col md={6} xxl={7} className={"text-start px-5 py-3 col-order"}>
+                            <h1>Casamentos</h1>
                             <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
                             <Link to={"/gallery"} >See more</Link>
                         </Col>
-                        <Col md={6} className={"px-5 py-3"}>
-                            <Image src={eventImage_casamento} rounded fluid />
+                        <Col md={6} xxl={5} className={"px-5 py-3"}>
+                            <Image src={eventImage_casamento} className={"shadow"} rounded fluid />
                         </Col>
                     </Row>
                 </Container>
             </section>
 
-            <section className={"py-5"}>
+            <section className={"py-5"} data-aos="fade-up-right">
                 <Container>
                     <Row className={"align-items-center"}>
-                        <Col md={6} className={"text-start px-5 py-3"}>
-                            <h2>Batizados</h2>
+                        <Col md={6} xxl={7} className={"text-start px-5 py-3"}>
+                            <h1>Batizados</h1>
                             <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
                             <Link to={"/gallery"} >See more</Link>
                         </Col>
-                        <Col md={6} className={"px-5 py-3"}>
-                            <Image src={eventImage_casamento} rounded fluid />
+                        <Col md={6} xxl={5} className={"px-5 py-3 col-order"}>
+                            <Image src={eventImage_casamento} className={"shadow"} rounded fluid />
                         </Col>
                     </Row>
                 </Container>
             </section>
 
-            <section className={"py-5"}>
+            <section className={"py-5"} data-aos="fade-up-left">
                 <Container>
                     <Row className={"align-items-center"}>
-                        <Col md={6} className={"text-start px-5 py-3"}>
-                            <h2>Aniversários</h2>
+                        <Col md={6} xxl={7} className={"text-start px-5 py-3 col-order"}>
+                            <h1>Aniversários</h1>
                             <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
                             <Link to={"/gallery"} >See more</Link>
                         </Col>
-                        <Col md={6} className={"px-5 py-3"}>
-                            <Image src={eventImage_casamento} rounded fluid />
+                        <Col md={6} xxl={5} className={"px-5 py-3"}>
+                            <Image src={eventImage_casamento} className={"shadow"} rounded fluid />
                         </Col>
                     </Row>
                 </Container>
             </section>
 
-            <section className={"py-5"}>
+            <section className={"py-5"} data-aos="fade-up-right">
                 <Container>
                     <Row className={"align-items-center"}>
-                        <Col md={6} className={"text-start px-5 py-3"}>
-                            <h2>Festas temáticas</h2>
+                        <Col md={6} xxl={7} className={"text-start px-5 py-3"}>
+                            <h1>Festas temáticas</h1>
                             <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
                             <Link to={"/gallery"} >See more</Link>
                         </Col>
-                        <Col md={6} className={"px-5 py-3"}>
-                            <Image src={eventImage_casamento} rounded fluid />
+                        <Col md={6} xxl={5} className={"px-5 py-3 col-order"}>
+                            <Image src={eventImage_casamento} className={"shadow"} rounded fluid />
                         </Col>
                     </Row>
                 </Container>
             </section>
 
-            <section className={" py-5"}>
+            <section className={"py-5"} data-aos="fade-up-left">
                 <Container>
                     <Row className={"align-items-center"}>
-                        <Col md={6} className={"text-start px-5 py-3"}>
-                            <h2>Eventos Corporativos</h2>
+                        <Col md={6} xxl={7} className={"text-start px-5 py-3 col-order"}>
+                            <h1>Eventos Corporativos</h1>
                             <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Nisi est sit amet facilisis magna etiam tempor. Proin fermentum leo vel orci porta non pulvinar.</p>
                             <Link to={"/gallery"} >See more</Link>
                         </Col>
-                        <Col md={6} className={"px-5 py-3"}>
-                            <Image src={eventImage_casamento} rounded fluid />
+                        <Col md={6} xxl={5} className={"px-5 py-3"}>
+                            <Image src={eventImage_casamento} className={"shadow"} rounded fluid />
                         </Col>
                     </Row>
                 </Container>
