971cec46b4051299281219ef2a2468fafa3d5a22
AndMJ <andrejoao070@gmail.com>
2023-07-13 02:56:42 +0100

    UPDATE Gallery page and styles

--- src/views/user/gallery/Gallery.jsx
+++ src/views/user/gallery/Gallery.jsx
@@ -2,56 +2,70 @@ import "./gallery.css";
 import {Col, Container, Row, Image} from "react-bootstrap";
 
 import appLogo from "/logo.svg"
+import eventImage_casamento from "../../../assets/img/events/casamento.jpg"
+
 function Gallery(){
     return(
         <>
-            <Container className={"mt-5"}>
-                <Row>
-                    <h1>Gallery</h1>
-                </Row>
-                <Row>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Image src={appLogo} rounded />
-                    </Col>
-                </Row>
-            </Container>
+            <section className={"my-3"}>
+                <Container className={"category py-5"}>
+                    <Row className={"title text-center mb-5"}>
+                        <h2 id={"Casamentos"}>Casamentos</h2>
+                    </Row>
+                    <Row className={"justify-content-evenly"}>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container className={"category my-3"}>
+                    <Row className={"title text-center mb-5"}>
+                        <h2 id={"Batizados"}>Batizados</h2>
+                    </Row>
+                    <Row className={"justify-content-evenly"}>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                        <Col xs={4} sm={4} md={3} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
+                            <Image src={eventImage_casamento} rounded fluid/>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </>
     )
 }
