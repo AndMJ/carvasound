cccaf49bd9c23a2fe7ebaf8b61d1d3e3b8f9a7c2
AndMJ <andrejoao070@gmail.com>
2023-07-13 01:25:59 +0100

    UPDATE now using React Suspense and removed animations testing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import('./App.css')
 import {Route, Routes} from "react-router-dom";
-import React, {useEffect} from 'react'
+import React, {Suspense} from 'react'
 
 //react boostrap components
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //my components/views
 //USER
-//import Layout from "./components/user/layout/Layout.jsx";
+// import Layout from "./components/user/layout/Layout.jsx";
 // import Home from "./views/user/home/Home.jsx";
 // import Events from "./views/user/events/Events.jsx";
 // import Gallery from "./views/user/gallery/Gallery.jsx";
@@ -24,25 +24,23 @@ const NotFound = React.lazy(()=> import("./views/user/not_found/NotFound.jsx"))
 //ADMIN
 // import AdminLayout from "./components/admin/layout/Layout.jsx";
 // import AdminHome from "./views/admin/home/Home.jsx";
+// import AdminLogin from "./views/admin/login/Login.jsx";
 
 const AdminLayout = React.lazy(()=> import("./components/admin/layout/Layout.jsx"))
 const AdminHome = React.lazy(()=> import("./views/admin/home/Home.jsx"))
 const AdminLogin = React.lazy(()=> import("./views/admin/login/Login.jsx"))
 
-import AOS from "aos";
-import 'aos/dist/aos.css';
 
-//TODO: implement React.Suspense - https://blog.logrocket.com/lazy-loading-components-in-react-16-6-6cea535c0b52/
+
+//TODO: implement React.Suspense properly - https://blog.logrocket.com/lazy-loading-components-in-react-16-6-6cea535c0b52/
 //      AOS animations not working properly
+//      restructure routes?, not sure
 
 function App() {
 
-    useEffect(()=>{
-        AOS.init()
-    },[]);
-
   return (
       <>
+      <Suspense fallback={<div>Loading...</div>}>
           <Routes>
               <Route path={"/"} element={<Layout />}>
                   <Route index element={<Home />} />
@@ -58,6 +56,11 @@ function App() {
               </Route>
               <Route path={"*"} element={<NotFound />}></Route>
           </Routes>
+          {/*<Routes>*/}
+          {/*    <Route to={"/"} element={<User/>}></Route>*/}
+          {/*    <Route to={"/admin"} element={<Admin/>}></Route>*/}
+          {/*</Routes>*/}
+      </Suspense>
       </>
   )
 }
