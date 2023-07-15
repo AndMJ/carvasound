c34752f094992d00f81bba92ccbe81952c4a4c48
AndMJ <andrejoao070@gmail.com>
2023-07-15 17:22:26 +0100

    UPDATE page title and modal to show image with a zoom in
    UPDATE page with framer-motion

--- src/views/user/gallery/Gallery.jsx
+++ src/views/user/gallery/Gallery.jsx
@@ -3,6 +3,25 @@ import {Col, Container, Row, Image} from "react-bootstrap";
 
 import eventImage_casamento from "../../../assets/img/events/casamento.jpg"
 import React, {useEffect, useState} from "react";
+import {FaHashtag} from "react-icons/fa";
+
+import { motion } from "framer-motion"
+
+const ImageModal = (props) => {
+    const handleEvent = (e) => {
+        /*checks if the click that is done, is not the image, so it doenst close
+        * just when outside the image is clicked*/
+        if (e.target.classList.contains("backdrop")){
+            props.setImgToModal(null)
+        }
+    }
+
+    return (
+        <div className={"backdrop"} onClick={handleEvent}>
+            <img src={props.ImgToModal} alt={"future image name or description"} />
+        </div>
+    )
+}
 
 function Gallery(){
     /*scroll to top to reset scroll*/
@@ -71,58 +90,50 @@ function Gallery(){
         }
     ])
 
+    const [ImgToModal, setImgToModal] = useState(null)
+
     return(
-        <React.Suspense fallback={<div>LOADING ...</div>}>
-            <section className={"my-5"}>
-                {/*<Container className={"category py-5"}>*/}
-                {/*    <Row className={"title text-center mb-5"}>*/}
-                {/*        <h2 id={"Casamentos"}>Casamentos</h2>*/}
-                {/*    </Row>*/}
-                {/*    <Row className={"justify-content-evenly"}>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*        <Col xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>*/}
-                {/*            <Image src={eventImage_casamento} rounded fluid/>*/}
-                {/*        </Col>*/}
-                {/*    </Row>*/}
-                {/*</Container>*/}
+        <>
+            <motion.section
+                className={"my-5"}
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                transition={{ delay: 0.5 }}
+            >
+
+                <Container className={"category mb-5"}>
+                    <Row className={"text-center"}>
+                        <h2>Gallery</h2>
+                    </Row>
+                </Container>
 
                 {GalleryImages.map(category => {
                     return (
                         <Container id={category.categoryName} key={category.categoryId} className={"category mb-5"}>
-                            <Row className={"title text-center mb-3"}>
-                                <h2>{category.categoryName}</h2>
+                            <Row className={"title mb-3 px-3"}>
+                                <h4> <FaHashtag style={ {color: "var(--custom-color-primary)"} }/> {category.categoryName} { (category.images.length === 0) && <span className={"badge badge bg-secondary"}>Images soon</span> }</h4>
                             </Row>
                             <Row className={"justify-content-evenly"}>
-                                {category.images.map(image => {
-                                    return (
-                                        <Col key={image.id} xs={6} sm={6} md={4} className={"p-3 p-sm-3 p-md-3 p-lg-4"}>
-                                            <Image src={image.path} rounded fluid/>
-                                        </Col>
-                                    )
-                                })}
+                                {
+                                    (category.images.length !== 0) &&
+                                        category.images.map(image => {
+                                            return (
+                                                <Col key={image.id} xs={12} sm={12} md={6} lg={4} className={"p-3"}>
+                                                    <Image src={image.path} rounded fluid onClick={()=>{setImgToModal(image.path)}} />
+                                                </Col>
+                                            )
+                                        })
+                                }
                             </Row>
                         </Container>
                     )
                 })}
-            </section>
-        </React.Suspense>
+
+            </motion.section>
+
+            {ImgToModal && <ImageModal ImgToModal={ImgToModal} setImgToModal={setImgToModal}></ImageModal>}
+
+        </>
     )
 }
 
