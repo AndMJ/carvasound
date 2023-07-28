84c4ce202c933a86e9434deb83d49203e1e028b3
AndMJ <andrejoao070@gmail.com>
2023-07-28 19:40:32 +0100

    UPDATE dashboard page

--- src/views/admin/dashboard/Dashboard.jsx
+++ src/views/admin/dashboard/Dashboard.jsx
@@ -1,11 +1,15 @@
 import "./dashboard.css"
 import {useEffect, useState} from "react";
-import {FaTable, FaUser} from "react-icons/fa";
+import {FaClock, FaPen, FaTable, FaUser} from "react-icons/fa";
 import {useOutletContext} from "react-router-dom";
+import {FaX} from "react-icons/fa6";
+
+import Clock from "../../../components/admin/Clock.jsx";
+import {Row} from "react-bootstrap";
 
 function Dashboard(){
 
-    const user = useOutletContext();
+    const [user, allUsers] = useOutletContext();
 
     return (
         <>
@@ -56,15 +60,73 @@ function Dashboard(){
                 {/*        </div>*/}
                 {/*    </div>*/}
                 {/*</div>*/}
-                <div className="card mb-4">
-                    <div className="card-header">
-                        <FaUser className={"me-1"}></FaUser>
-                        card title
+
+                <Row>
+                    <div className={"col-xl-3 col-md-6"}>
+                        <div className="card mb-4">
+                            <div className="card-header">
+                                <FaUser className={"me-1"}></FaUser> Bem vindo
+                            </div>
+                            <div className="card-body">
+                                 {user ? user.name : "user"}
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-body">
-                        Bem vindo, {user ? user.name : "user"}
+
+                    <div className={"col-xl-3 col-md-6"}>
+                        <div className="card mb-4">
+                            <div className="card-header">
+                                <FaClock className={"me-1"}></FaClock> Today&apos;s date
+                            </div>
+                            <div className="card-body">
+                                <Clock />
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </Row>
+
+                <Row>
+                    <div className={"col"}>
+                        <div className="card mb-4">
+                            <div className="card-header">
+                                <FaTable className={"me-1"}></FaTable> Table users
+                            </div>
+                            <div className="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Permissions</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {allUsers ?
+                                        allUsers.map(userdata => {
+                                            return (
+                                                <>
+                                                    <tr>
+                                                        <td>{userdata.id}</td>
+                                                        <td>{userdata.name}</td>
+                                                        <td>{userdata.email}</td>
+                                                        <td>{userdata.permissions}</td>
+                                                    </tr>
+                                                </>
+                                            )
+                                        })
+
+                                        :
+
+                                        <tr>no data found</tr>
+                                    }
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </Row>
             </div>
         </>
     )
