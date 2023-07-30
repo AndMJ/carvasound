017681c614a93d414901fd72888953364daad6f0
AndMJ <andrejoao070@gmail.com>
2023-07-30 01:00:13 +0100

    ADD appwrite.config

--- src/appwrite/appwrite.config.jsx
+++ src/appwrite/appwrite.config.jsx
@@ -1,9 +1,17 @@
-import { Client } from 'appwrite';
+import {Account, Client, Databases} from 'appwrite';
+
+export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const DATABASE_ID = import.meta.env.VITE_DB_PRODUCTION_ID;
+export const COLLECTION_ID_GALLERY = import.meta.env.VITE_COLLECTION_GALLERY;
+
 
 const client = new Client();
 
 client
-    .setEndpoint(import.meta.env.END_POINT)
-    .setProject(import.meta.env.PROJECT);
+    .setEndpoint(import.meta.env.VITE_END_POINT)
+    .setProject(PROJECT_ID);
+
+export const database = new Databases(client);
+export const account = new Account(client);
 
 export default client;
