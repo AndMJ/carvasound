33884738e383d46ac707a53a2d1574b3112ca432
AndMJ <andrejoao070@gmail.com>
2023-09-08 17:05:58 +0100

    FIX loader styles

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,10 +1,13 @@
 import "./Loader.css"
+import React from "react";
+React.lazy(()=> import('bootstrap/dist/css/bootstrap.min.css'))
+
 
 const Loader = () => {
     return (
         <>
             <div className="loader-spinner spinner-border position-absolute top-50 start-50" role="status">
-                <span className="visually-hidden">Loading...</span>
+                <span className="visually-hidden" hidden>Loading...</span>
             </div>
         </>
     )
