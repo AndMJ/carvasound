4928e1255def545d1f28f6a201d068070d7c0243
AndMJ <andrejoao070@gmail.com>
2023-10-15 16:05:29 +0100

    UPDATE exporting method for gallery listing

--- src/utils/authContext.jsx
+++ src/utils/authContext.jsx
@@ -211,6 +211,7 @@ export const AuthProvider = ({children}) => {
 
         getStorageImagesByID,
         getGalleryList,
+        getCategoryList,
         getCategoryByID,
 
         deleteGalleryByID,
