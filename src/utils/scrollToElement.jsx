378ab085ec01fc27359a1a36e41c0820fcc8738e
AndMJ <andrejoao070@gmail.com>
2023-10-25 02:57:17 +0100

    ADD scroll offset

--- src/utils/scrollToElement.jsx
+++ src/utils/scrollToElement.jsx
@@ -2,8 +2,10 @@ export const scrollToElement = (elementId) => {
     const element = document.getElementById(elementId);
 
     if (element) {
+        let offset = 50 //navbar height
+
         // Calculate the vertical position of the element
-        const offsetTop = element.getBoundingClientRect().top + window.scrollY;
+        const offsetTop = (element.getBoundingClientRect().top + window.scrollY) - offset;
 
         // Scroll to the element's position
         window.scrollTo({
