0f40fb0b8b8385b965ef7655ffe1f48a14034c06
AndMJ <andrejoao070@gmail.com>
2023-11-28 23:33:23 +0000

    UPDATE loader to display correctly

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,9 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./Loader.css"
 
 const Loader = () => {
     return (
         <>
-            <div className="loader-spinner spinner-border position-absolute top-50 start-50" role="status"></div>
+            <div className={"vw-100 vh-100"}>
+                <div className="loader-spinner spinner-border position-absolute top-50 start-50" role="status"></div>
+            </div>
         </>
     )
 }
