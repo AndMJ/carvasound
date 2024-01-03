3486c4144de811fa9fec8ba387b018860b9def86
AndMJ <andrejoao070@gmail.com>
2024-01-03 20:43:32 +0000

    UPDATE started category section

--- src/views/admin/category/Category.jsx
+++ src/views/admin/category/Category.jsx
@@ -1,40 +1,52 @@
 import "./category.css"
-import {FaImages, FaTable} from "react-icons/fa";
+import {FaImages, FaList, FaTable} from "react-icons/fa";
+
+import {useAuth} from "../../../utils/authContext.jsx";
+import {useEffect, useState} from "react";
+import {FaX} from "react-icons/fa6";
+
 
 const Category = () => {
+    /*TODO: on parent caller(admin/Gallery.jsx), create useState var for category storing, so its accessible from everywhere, fileupload.jsx inclusive*/
+
+    const {getCategoryList, getCategoryByID} = useAuth();
+
+    const [categoriesList, setCategoriesList] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            const categories = await getCategoryList();
+            setCategoriesList(categories.documents)
+        })();
+    }, []);
+
     return (
         <>
-            {/*// <!-- Page Heading -->*/}
-            <div className=" mb-4"> {/*d-sm-flex align-items-center justify-content-between*/}
-                <h1 className="h3 mb-1 text-gray-800">Category</h1>
-                <p className="mb-4">Categorize your gallery.</p>
-            </div>
+            <div className="col-lg-4">
+                <div className="card shadow mb-4">
+                    <div className="card-header py-3 d-flex justify-content-between align-items-center">
+                        <h6 className="m-0 font-weight-bold text-primary"><span><FaList className={"me-1"}></FaList></span> New category</h6>
+                        <a href="#" className=" btn btn-success btn-sm text-white">Add</a>
+                    </div>
+                    <div className="card-body">
+                        <div className="row g-1">
+                            {categoriesList?.map(category => {
+                                return (
+                                    <>
+                                        <div className="col-auto">
+                                            <a className={"btn btn-primary btn-icon-split text-white"}>
+                                                <span onClick={() => alert("delete")} className={"icon"}><FaX></FaX></span>
+                                                <span className={"text"}>{category.name}</span>
+                                            </a>
+                                        </div>
+                                    </>
+                                )
+                            })}
 
-            {/*// <!-- Content Row -->*/}
-            <div className="row">
+                        </div>
 
-                <div className="col-lg-12">
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-primary"><span><FaImages className={"me-1"}></FaImages></span> Upload images</h6>
-                        </div>
-                        <div className="card-body">
-                            categ
-                        </div>
                     </div>
                 </div>
-
-                <div className="col-lg-12">
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-primary"><span><FaTable className={"me-1"}></FaTable></span> Gallery table</h6>
-                        </div>
-                        <div className="card-body gallery-table">
-                            table
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </>
     )
