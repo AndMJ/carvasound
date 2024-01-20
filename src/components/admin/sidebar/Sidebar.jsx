2f49bd6bf876fc5569b5c7245aa7ea7406815826
AndMJ <andrejoao070@gmail.com>
2024-01-20 19:01:45 +0000

    UPDATE disabled dashboard sidebar button

--- src/components/admin/sidebar/Sidebar.jsx
+++ src/components/admin/sidebar/Sidebar.jsx
@@ -50,34 +50,41 @@ const Sidebar = () => {
             <Nav className={"navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"} id="accordionSidebar" as="ul" activeKey={url}> {/*+ (toggleClass ? "toggled" : "")*/}
 
                 {/*// <!-- Sidebar - Brand -->*/}
-                <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div className="sidebar-brand d-flex align-items-center justify-content-center">
                     <div className="sidebar-brand-icon">
                         <img src={appLogo} width={40} />
                     </div>
                     <div className="sidebar-brand-text mx-3">Carvasound</div>
-                </a>
+                </div>
+
+                {/*<Nav.Link as={Link} to={"/admin"} eventKey={"/admin"} className={"sidebar-brand d-flex align-items-center justify-content-center"}>
+                    <div className="sidebar-brand-icon">
+                        <img src={appLogo} width={40}/>
+                    </div>
+                    <div className="sidebar-brand-text mx-3">Carvasound</div>
+                </Nav.Link>*/}
 
                 {/*// <!-- Divider -->*/}
                 <hr className="sidebar-divider my-0"/>
 
                 {/*// <!-- Nav Item - Dashboard -->*/}
-                <Nav.Item as="li">
+                {/*<Nav.Item as="li">
                     <Nav.Link as={Link} to={"/admin"} eventKey={"/admin"}>
                         <FaTachometerAlt className={"icon"}></FaTachometerAlt> <span>Dashboard</span>
                     </Nav.Link>
-                </Nav.Item>
+                </Nav.Item>*/}
 
                 {/*// <!-- Divider -->*/}
-                <hr className="sidebar-divider"/>
+                {/*<hr className="sidebar-divider"/>*/}
 
                 {/*// <!-- Heading -->*/}
-                <div className="sidebar-heading">
+                {/*<div className="sidebar-heading">
                     Images
-                </div>
+                </div>*/}
 
                 {/*// <!-- Nav Item - Pages Collapse Menu -->*/}
                 <Nav.Item as="li">
-                    <Nav.Link as={Link} to={"/admin/gallery"} eventKey={"/admin/gallery"}>
+                    <Nav.Link as={Link} to={"/admin"} eventKey={"/admin"}> {/*to={"/admin/gallery"} eventKey={"/admin/gallery"}*/}
                         <FaImage className={"icon"}></FaImage> <span>Gallery</span>
                     </Nav.Link>
                     {/*<Nav.Link as={Link} to={"/admin/category"} eventKey={"/admin/category"}>
