48fe5b367d3020412d8f2314b2754a84b2c9738a
AndMJ <andrejoao070@gmail.com>
2024-01-24 02:33:46 +0000

    UPDATE started changes to avatar dropdown

--- src/components/admin/header/Header.jsx
+++ src/components/admin/header/Header.jsx
@@ -1,28 +1,21 @@
 import "./header.css"
 
-import {FaBars} from "react-icons/fa";
 import {useAuth} from "../../../utils/authContext.jsx";
 import {useEffect, useState} from "react";
 
 import user_avatar from "../../../assets/img/avatar/avatar.png"
 import MenuIcon from "@mui/icons-material/Menu.js";
-import {IconButton} from "@mui/material";
+import {Avatar, Divider, IconButton, ListItemIcon, Menu, MenuItem, Typography} from "@mui/material";
+import {Logout, Settings} from "@mui/icons-material";
 
 const Header = ({handleDrawerToggle}) => {
     const {user, handleLogout} = useAuth()
-    const handleEvent = (e) => {
-        e.preventDefault();
-        //document.body.classList.toggle('sidebar-toggled');
-        document.getElementsByClassName("sidebar")[0].classList.toggle('toggled');
-    }
-
 
     const [confirm, setConfirm] = useState(false)
     const handleLogoutClick = () => {
         setConfirm(true)
     }
-
-    /*useEffect(() => { //on dropdown hide, reset logout confirmation
+    useEffect(() => { //on dropdown hide, reset logout confirmation
         const userDropdown = document.getElementById('userDropdown')
         const handleEventListener = () => {
             setConfirm(false)
@@ -32,7 +25,17 @@ const Header = ({handleDrawerToggle}) => {
         return () => {
             userDropdown.removeEventListener('scroll', handleEventListener);
         };
-    }, []);*/
+    }, []);
+
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
@@ -49,7 +52,7 @@ const Header = ({handleDrawerToggle}) => {
                     //edge="start"
                     onClick={handleDrawerToggle}
                     sx={{
-                        ml: 1,
+                        ml: 2,
                         //display: { lg: 'none' }
                     }}
                 >
@@ -82,6 +85,70 @@ const Header = ({handleDrawerToggle}) => {
                     </li>
 
                 </ul>
+
+                <Typography>{user ? user.name : "user"}</Typography>
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 1, mr: 2 }}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                    <Avatar sx={{ width: 32, height: 32 }}></Avatar>
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&::before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Avatar /> My account
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
 
             </nav>
         </>
